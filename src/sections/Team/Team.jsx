5672dbd0c3a4be7c4ddd8e3bda7537b6c83b1391
ian <ian@samespace.com>
2023-03-11 16:52:39 +0530

    Team section Done

--- src/sections/Team/Team.jsx
+++ src/sections/Team/Team.jsx
@@ -23,12 +23,16 @@ const members = [
 const Team = () => {
   return (
     <div className={styles.container}>
-      {members.map((item) => (
-        <div className={styles.member}>
-          <img src="https://www.mordeo.org/files/uploads/2021/03/The-Batman-DC-Darkness-2021-Poster-4K-Ultra-HD-Mobile-Wallpaper-scaled.jpg" />
-          <div className={styles.name}>{item.name}</div>
-        </div>
-      ))}
+      <div className={styles.title}>Meet The Team</div>
+
+      <div className={styles.subContainer}>
+        {members.map((item) => (
+          <div className={styles.member}>
+            <img src="https://www.mordeo.org/files/uploads/2021/03/The-Batman-DC-Darkness-2021-Poster-4K-Ultra-HD-Mobile-Wallpaper-scaled.jpg" />
+            <div className={styles.name}>{item.name}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
